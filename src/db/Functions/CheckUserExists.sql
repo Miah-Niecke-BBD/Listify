bdbfd496e65e22d9237eb1c0819aef1ee5eef7fb
Azael-BBD <Azael.Letsoalo@bbd.co.za>
2025-02-13 18:18:27 +0200

    Update CheckUserExists.sql

--- src/db/Functions/CheckUserExists.sql
+++ src/db/Functions/CheckUserExists.sql
@@ -3,7 +3,11 @@ RETURNS BIT
 AS
 BEGIN
     DECLARE @exists BIT = 0;
+    IF @gitHubID IS NULL RETURN 0;
+
     IF EXISTS (SELECT 1 FROM Users WHERE gitHubID = @gitHubID)
         SET @exists = 1;
+
     RETURN @exists;
 END;
+GO
