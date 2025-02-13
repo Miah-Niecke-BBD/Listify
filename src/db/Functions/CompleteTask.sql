496a973ca2ac2e889e07553b60473e03bd49c858
Azael-BBD <Azael.Letsoalo@bbd.co.za>
2025-02-13 18:08:33 +0200

    Update CompleteTask.sql

--- src/db/Functions/CompleteTask.sql
+++ src/db/Functions/CompleteTask.sql
@@ -1,8 +1,14 @@
-create Procedure CompleteTask
-@taskID INT 
+CREATE FUNCTION dbo.UserExists(@gitHubID VARCHAR(2083))
+RETURNS BIT
 AS
-Begin
-update Tasks
-set dateCompletes = GETDATE()
-Where taskID = @taskID;
+BEGIN
+    DECLARE @exists BIT = 0;
+
+    IF @gitHubID IS NULL RETURN 0;
+
+    IF EXISTS (SELECT 1 FROM Users WHERE gitHubID = @gitHubID)
+        SET @exists = 1;
+
+    RETURN @exists;
 END;
+GO
