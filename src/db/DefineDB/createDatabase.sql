8580dd589ff6caeb5eaed1b08b1b0575be807f83
NadrianBBD <Nadrian.Potonas@bbd.co.za>
2025-02-13 07:49:43 +0200

    Update createDatabase.sql

--- src/db/DefineDB/createDatabase.sql
+++ src/db/DefineDB/createDatabase.sql
@@ -23,7 +23,7 @@ CREATE TABLE [Teams] (
 GO
 
 CREATE TABLE [TeamMembers] (
-  [teamMemberID] INT PRIMARY KEY NOT NULL,
+  [teamMemberID] INT PRIMARY KEY IDENTITY(1, 1),
   [userID] INT NOT NULL,
   [teamID] INT NOT NULL,
   [isTeamLeader] BIT NOT NULL DEFAULT 0,
@@ -33,7 +33,7 @@ CREATE TABLE [TeamMembers] (
 GO
 
 CREATE TABLE [Projects] (
-  [projectID] INT PRIMARY KEY NOT NULL IDENTITY(1, 1),
+  [projectID] INT PRIMARY KEY IDENTITY(1, 1),
   [teamID] INT NOT NULL,
   [projectName] VARCHAR(100) NOT NULL,
   [projectDescription] VARCHAR(500),
@@ -72,7 +72,7 @@ CREATE TABLE [Tasks] (
 GO
 
 CREATE TABLE [TaskAssignees] (
-  [taskAssigneeID] INT PRIMARY KEY NOT NULL,
+  [taskAssigneeID] INT PRIMARY KEY NOT NULL IDENTITY(1, 1),
   [userID] INT NOT NULL,
   [taskID] INT NOT NULL,
   FOREIGN KEY ([userID]) REFERENCES [Users] ([userID]),
@@ -81,7 +81,7 @@ CREATE TABLE [TaskAssignees] (
 GO
 
 CREATE TABLE [TaskDependencies] (
-  [taskDependencyID] INT PRIMARY KEY NOT NULL,
+  [taskDependencyID] INT PRIMARY KEY NOT NULL IDENTITY(1, 1),
   [taskID] INT NOT NULL,
   [dependentTaskID] INT NOT NULL,
   FOREIGN KEY ([taskID]) REFERENCES [Tasks] ([taskID]),
@@ -90,7 +90,7 @@ CREATE TABLE [TaskDependencies] (
 GO
 
 CREATE TABLE [ProjectAssignees] (
-  [projectAssigneeID] INT PRIMARY KEY NOT NULL,
+  [projectAssigneeID] INT PRIMARY KEY NOT NULL IDENTITY(1, 1),
   [userID] INT NOT NULL,
   [projectID] INT NOT NULL,
   FOREIGN KEY ([projectID]) REFERENCES [Projects] ([projectID]),
