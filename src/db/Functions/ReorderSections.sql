b1275b9d293180475332dfa816cff6c5c5543f05
Azael-BBD <Azael.Letsoalo@bbd.co.za>
2025-02-13 18:38:01 +0200

    Update ReorderSections.sql

--- src/db/Functions/ReorderSections.sql
+++ src/db/Functions/ReorderSections.sql
@@ -6,20 +6,18 @@ AS
 BEGIN
     BEGIN TRANSACTION
     BEGIN TRY
-        -- Validate section existence
+    
         IF NOT EXISTS (SELECT 1 FROM Sections WHERE sectionID = @sectionID AND projectID = @projectID)
         BEGIN
             THROW 50022, 'Section does not exist in given project', 1;
         END;
 
-        -- Shift sections forward, but avoid affecting the moving section itself
         UPDATE Sections
         SET sectionPosition = sectionPosition + 1
         WHERE projectID = @projectID 
         AND sectionPosition >= @newPosition 
         AND sectionID <> @sectionID;
 
-        -- Assign the new position to the moving section
         UPDATE Sections
         SET sectionPosition = @newPosition, updatedAt = GETDATE()
         WHERE sectionID = @sectionID;
