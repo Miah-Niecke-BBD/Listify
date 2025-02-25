469f33da3ff0cb6d4996d40cf4455e88b12ef895
Kamogelo Tsatsi <kamogelo.tsatsi@bbd.co.za>
2025-02-25 10:50:53 +0200

    Removed unused code

--- API/src/main/java/org/setup/Listify/Assembler/UserAssembler.java
+++ API/src/main/java/org/setup/Listify/Assembler/UserAssembler.java
@@ -1,23 +1,23 @@
-    package org.setup.Listify.Assembler;
-
-    import org.setup.Listify.Controller.GitHubOAuthController;
-    import org.setup.Listify.Model.Users;
-    import org.springframework.hateoas.Link;
-    import org.springframework.hateoas.server.RepresentationModelAssembler;
-    import org.springframework.hateoas.EntityModel;
-    import org.springframework.stereotype.Component;
-
-    import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
-
-    @Component
-    public class UserAssembler implements RepresentationModelAssembler<Users, EntityModel<Users>> {
-
-        @Override
-        public EntityModel<Users> toModel(Users user) {
-
-            Link selfLink = linkTo(GitHubOAuthController.class)
-                    .slash("login")
-                    .withSelfRel();
-            return EntityModel.of(user, selfLink);
-        }
-    }
+//    package org.setup.Listify.Assembler;
+//
+//    import org.setup.Listify.Controller.GitHubOAuthController;
+//    import org.setup.Listify.Model.Users;
+//    import org.springframework.hateoas.Link;
+//    import org.springframework.hateoas.server.RepresentationModelAssembler;
+//    import org.springframework.hateoas.EntityModel;
+//    import org.springframework.stereotype.Component;
+//
+//    import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+//
+//    @Component
+//    public class UserAssembler implements RepresentationModelAssembler<Users, EntityModel<Users>> {
+//
+//        @Override
+//        public EntityModel<Users> toModel(Users user) {
+//
+//            Link selfLink = linkTo(GitHubOAuthController.class)
+//                    .slash("login")
+//                    .withSelfRel();
+//            return EntityModel.of(user, selfLink);
+//        }
+//    }
--- API/src/main/java/org/setup/Listify/Config/SecurityConfig.java
+++ API/src/main/java/org/setup/Listify/Config/SecurityConfig.java
@@ -1,33 +1,33 @@
-package org.setup.Listify.Config;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/", "/login", "/index").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .permitAll()
-                )
-                .oauth2Login(oauth2 -> oauth2
-                        .defaultSuccessUrl("/home", true)
-                        .failureUrl("/login?error=true")
-                );
-
-        return http.build();
-    }
-}
-
-
-
+//package org.setup.Listify.Config;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers("/", "/login", "/index").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .formLogin(form -> form
+//                        .loginPage("/login")
+//                        .permitAll()
+//                )
+//                .oauth2Login(oauth2 -> oauth2
+//                        .defaultSuccessUrl("/home", true)
+//                        .failureUrl("/login?error=true")
+//                );
+//
+//        return http.build();
+//    }
+//}
+//
+//
+//
