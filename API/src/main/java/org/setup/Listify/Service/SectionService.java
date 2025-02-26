e9beca2e3dd41db9cdfdc2772fb5bddb09ceec34
Azael-BBD <Azael.Letsoalo@bbd.co.za>
2025-02-26 13:38:52 +0200

    Update SectionService.java

--- API/src/main/java/org/setup/Listify/Service/SectionService.java
+++ API/src/main/java/org/setup/Listify/Service/SectionService.java
@@ -7,10 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
-<<<<<<< Updated upstream
 import java.util.Optional;
-=======
->>>>>>> Stashed changes
 
 @Service
 public class SectionService {
