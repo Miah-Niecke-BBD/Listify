bd5bcc6b12dd91fdc597cd47c9a25a4860e4e4d0
Miah Niecke Dreeckmeier <Miah.Dreeckmeier@bbd.co.za>
2025-02-27 17:35:51 +0200

    Merged in main

--- API/src/main/java/org/setup/Listify/ListifyApplication.java
+++ API/src/main/java/org/setup/Listify/ListifyApplication.java
@@ -1,15 +1,14 @@
 package org.setup.Listify;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "ListifyAPI", version = "1.0", description = "API for Listify productivity tool"))
 public class ListifyApplication {
-
-
     public static void main(String[] args) {
         SpringApplication.run(ListifyApplication.class, args);
     }
-
-
 }
