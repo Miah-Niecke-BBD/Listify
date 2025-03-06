6385737b75f42fccc066dc022acbb8b761891824
Miah Niecke Dreeckmeier <Miah.Dreeckmeier@bbd.co.za>
2025-03-06 11:38:37 +0200

    Updated procedures

--- db/Procedures/Insert/uspCreateMyProjectsTeam.sql
+++ db/Procedures/Insert/uspCreateMyProjectsTeam.sql
@@ -1,36 +1,43 @@
-
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'uspCreateMyProjectsTeam')
+BEGIN
+    DROP PROCEDURE listify.uspCreateMyProjectsTeam;
+END;
+ 
+GO
+ 
 CREATE PROCEDURE listify.uspCreateMyProjectsTeam
     @userID INT, 
     @gitHubID VARCHAR(2083) 
 AS
 BEGIN
     DECLARE @teamID INT, @projectID INT;
-
+ 
     BEGIN TRY
-        
-
         INSERT INTO listify.Teams (teamName, createdAt)
-        VALUES ('MyProjects', SYSDATETIME());
-
+        VALUES ('MyProjects', SYSDATETIMEOFFSET());
+ 
         SET @teamID = SCOPE_IDENTITY();
-     
         INSERT INTO listify.TeamMembers (userID, teamID, isTeamLeader)
         VALUES (@userID, @teamID, 1);
-
+ 
         INSERT INTO listify.Projects (teamID, projectName, projectDescription, createdAt)
-        VALUES (@teamID, 'MyList', 'Random Tasks', SYSDATETIME());
-
-        INSERT INTO listify.Projects (teamID, projectName, projectDescription, createdAt)
-        VALUES (@teamID, 'Home','Default personal project', SYSDATETIME());
-
+        VALUES (@teamID, 'MyList', 'Random Tasks', SYSDATETIMEOFFSET());
+ 
         SET @projectID = SCOPE_IDENTITY();
-
+ 
         INSERT INTO listify.ProjectAssignees (userID, projectID)
         VALUES (@userID, @projectID);
-
+ 
+        INSERT INTO listify.Projects (teamID, projectName, projectDescription, createdAt)
+        VALUES (@teamID, 'Home','Default personal project', SYSDATETIMEOFFSET());
+ 
+        SET @projectID = SCOPE_IDENTITY();
+ 
+        INSERT INTO listify.ProjectAssignees (userID, projectID)
+        VALUES (@userID, @projectID);
+ 
     END TRY
-    BEGIN CATCH
-        PRINT 'Error occured:' + ERROR_MESSAGE();
-        THROW;
+    BEGIN CATCH    
+        THROW 50050, 'Could not create the default projects', 1;
     END CATCH
 END;
