accfe630f63172443ec609cf908320a9f7c7a86a
Nadrian Potonas <Nadrian.Potonas@bbd.co.za>
2025-03-22 17:33:46 +0200

    disable csrf token

--- api/src/main/java/org/listify/config/SecurityConfig.java
+++ api/src/main/java/org/listify/config/SecurityConfig.java
@@ -7,11 +7,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizationCodeGrantFilter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
-
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -23,16 +24,14 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
                         .requestMatchers("/login", "/oauth2/callback/google", "/authentication", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
 
                 ).cors(Customizer.withDefaults())
                 .addFilterBefore(jwtTokenFilter, OAuth2AuthorizationCodeGrantFilter.class)
-                .csrf()
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler());
-
+                .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
