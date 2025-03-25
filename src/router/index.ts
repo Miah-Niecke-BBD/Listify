2b68c5fd198f5eeca30360baf779aa8f1c4f39ae
Kamogelo Tsatsi <kamogelo.tsatsi@bbd.co.za>
2025-03-25 16:15:51 +0200

    Added route to project

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
 import LoadingView from '../views/LoadingView.vue'
+import ProjectView from '@/views/ProjectView.vue'
+import HomeView from "@/views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,7 +16,7 @@ const router = createRouter({
     {
       path: '/loading',
       name: 'loading',
-      component: LoadingView, 
+      component: LoadingView,
     },
     {
       path: '/home',
@@ -23,12 +25,17 @@ const router = createRouter({
       beforeEnter: (to, from, next) => {
         const jwtToken = localStorage.getItem('jwtToken');
         if (jwtToken) {
-          next(); 
+          next();
         } else {
-          next('/'); 
+          next('/');
         }
       }
     },
+    {
+      path: '/project/:id',
+      name: "project",
+      component: ProjectView
+    }
   ],
 })
 
