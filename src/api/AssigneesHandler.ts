da2670d8f789d3dd2eb480a6523e82bf3d636398
Kamogelo Tsatsi <kamogelo.tsatsi@bbd.co.za>
2025-03-25 22:47:27 +0200

    Added Task Assignee feature

---
+++ src/api/AssigneesHandler.ts
@@ -0,0 +1,60 @@
+import type {Assignee} from "@/models/Assignee.ts";
+import { jwtToken } from "@/models/JWTToken";
+const API_BASE_URL = "http://localhost:8080";
+
+
+export default class AssigneesHandler {
+  static async assignTask(taskID : number, githubID: string): Promise<Assignee | null> {
+    try {
+      const response = await fetch(`${API_BASE_URL}/tasks/assignee/${taskID}?githubID=${encodeURIComponent(githubID)}`,
+        {
+          method: 'POST',
+          headers: {
+            Accept: "*/*",
+            Authorization: `Bearer ${jwtToken}`
+          },
+        }
+      )
+
+      if (!response.ok) {
+        const errorMessage = await response.text();
+        throw new Error(`Failed to assign task: ${errorMessage}`);
+      }
+
+      const taskAssignee = await response.json()
+      return {
+        taskAssigneeID: taskAssignee.taskAssigneeID,
+        userID: taskAssignee.userID,
+        taskID: taskAssignee.taskID
+      }
+    } catch (error) {
+      console.error("Error assigning task:", error);
+      return null;
+    }
+  }
+
+
+  static async deleteAssignment(taskID: number, githubID: string): Promise<boolean> {
+    try {
+      const response = await fetch(`${API_BASE_URL}/tasks/assignee/${taskID}?githubID=${encodeURIComponent(githubID)}`, {
+          method: "DELETE",
+          headers: {
+            Accept: "*/*",
+            Authorization: `Bearer ${jwtToken}`
+          },
+        }
+      )
+
+      if (!response.ok) {
+        const errorMessage = await response.text();
+        throw new Error(`Failed to unassign task: ${errorMessage}`);
+      }
+      return true
+    } catch (error) {
+      console.error("Error unassigning task:", error);
+      return false
+    }
+  }
+
+
+}
